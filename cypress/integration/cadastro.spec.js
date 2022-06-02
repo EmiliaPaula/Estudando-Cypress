c1b069e981f5f581627eef755b1e4df39c1c810d
EmiliaPaula <emiliaand_paula1@msn.com>
2022-06-02 02:07:41 -0300

    Checkpoint na automação web

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,21 @@
+describe('cadastro', ()=> {
+  it('Usuário de se tornar um entregador', ()=>{
+    cy.viewport(1440, 900)
+    cy.visit('https://buger.eats.vercel.app')
+
+    cy.get('a[href="/deliver"]').click()
+    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+    var entregador = {
+      nome: 'Emília',
+      cpf: '00000014141',
+      email:'testando@hotmail.com',
+      whatsapp:'11999999999'
+    }
+
+    cy.get('input[name="nome"]').type(entregador.nome)
+    cy.get('input[name="cpf"]').type(entregador.cpf)
+    cy.get('input[name="email"]').type(entregador.email)
+    cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+  })
+})
