85f375c5f596117fe19440ace13e0965560f4af1
EmiliaPaula <emiliaand_paula1@msn.com>
2022-06-06 01:07:06 -0300

    Factory e Faker com dados e CPF dinâmicos

--- cypress/pages/SignupPage.js
+++ cypress/pages/SignupPage.js
@@ -6,7 +6,7 @@ class SignupPage {
     cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
   }
   fillForm(deliver){
-    cy.get('input[name="nome"]').type(deliver.name)
+    cy.get('input[name="fullName"]').type(deliver.name)
     cy.get('input[name="cpf"]').type(deliver.cpf)
     cy.get('input[name="email"]').type(deliver.email)
     cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
@@ -15,13 +15,13 @@ class SignupPage {
     cy.get('input[type="button"][value="Buscar CEP"]').click
 
     cy.get('input[name="address-number]').type(deliver.address.number)
-    cy.get('input[name="address-details]').type(delivery.address.details)
+    cy.get('input[name="address-details]').type(deliver.address.details)
 
     cy.get('input[name="address"]').should('have.value', deliver.address.street)
     cy.get('input[name="district"]').should('have.value', deliver.address.district)
-    cy.get('input[name="city-uf]').should('have.value', deliver.address.city_state)
+    cy.get('input[name="city-state]').should('have.value', deliver.address.city_state)
 
-    cy.contains('.delivery-method li', delivery.delivery_method).click
+    cy.contains('.delivery-method li', deliver.delivery_method).click
     cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
   }
 
