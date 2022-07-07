196bc9812f34f3b573108ce5a52689b5209702e1
EmiliaPaula <emiliaand_paula1@msn.com>
2022-07-07 14:09:11 -0300

    criando nova modelagem de step by step

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,5 +1,6 @@
 import signup from '../pages/SignupPage'
 import signupFactory from '..factories/SignupFactory'
+import signupPage from '../pages/SignupPage'
 
 describe('Signup', ()=> {
 
@@ -41,6 +42,30 @@ describe('Signup', ()=> {
     signup.submit()
     signup.alertMessageShouldBe('Oops! Email com formato inválido.') 
   })
+
+  context('Required fields', function(){
+    const messages = [
+      {field: 'name', output: 'É necessário informar o nome'},
+      {field: 'cpf', output: 'É necessário informar o CPF'},
+      {field: 'email', output: 'É necessário informar o email'},
+      {field: 'postalcode', output: 'É necessário informar o CEP'},
+      {field: 'number', output: 'É necessário informar o número do endereço'},
+      {field: 'delivery_method', output: 'Selecione o método de entrega'},
+      {field: 'cnh', output: 'Adicione uma foto da sua CNH'}
+    ]
+
+    before(function(){
+      signupPage.go()
+      signupPage.submit()
+    })
+
+    messages.forEach(function(msg){
+      it(`${msg.field} is required`, function(){
+          signupPage.alertMessageShouldBe(msg.output)
+      })
+    })
+  })
+
 })
 
 // **User should be = Usuário deve se tornar um entregador
